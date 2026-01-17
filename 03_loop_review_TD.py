1e8446bf37cf1b7b04cdb2c86c452787c2216b08
Tim Daehnert <tddaehner@coastal.edu>
2026-01-17 17:15:36 -0500

    Initial Commit

---
+++ 03_loop_review_TD.py
@@ -0,0 +1,15 @@
+"""
+    Program 3: Loop Review
+    Student Name: Timothy Daehnert
+    Course: CSCI 409 D1
+"""
+
+# 1. Write a FOR loop to iterate over the airports list
+airports = ["MYR", "MCO", "ATL", "LGA"]
+for a in airports:
+    print(a)
+
+# 2. Use the range function to loop through the code 5 times
+for x in range(5):
+    for item in airports:
+        print(item)
